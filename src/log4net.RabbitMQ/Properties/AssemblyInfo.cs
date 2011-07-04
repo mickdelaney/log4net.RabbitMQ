7990449ba99fc50bae827d33e0904f46e32b07ea
Henrik <henrik@haf.se>
2011-07-04 12:25:20 +0200

    release 0.1

--- src/log4net.RabbitMQ/Properties/AssemblyInfo.cs
+++ src/log4net.RabbitMQ/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("log4net.RabbitMQ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An assembly with a log4net appender for RabbitMQ / AMQP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Logibit")]
 [assembly: AssemblyProduct("log4net.RabbitMQ")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
