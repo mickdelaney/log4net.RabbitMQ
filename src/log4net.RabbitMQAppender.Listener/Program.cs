1a6219dfe6a412fbb46715f038f0b40c7733e80a
Henrik <henrik@haf.se>
2011-07-04 12:53:00 +0200

    added ex listener

---
+++ src/log4net.RabbitMQAppender.Listener/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace log4net.RabbitMQAppender.Listener
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory
+			{
+				HostName = "localhost",
+				UserName = "guest",
+				Password = "guest",
+				Protocol = Protocols.DefaultProtocol
+			};
+
+			using (var c = factory.CreateConnection())
+			using (var m = c.CreateModel())
+			{
+				var consumer = new QueueingBasicConsumer(m);
+				var q = m.QueueDeclare("", false, true, true, null);
+				m.QueueBind(q, "log4net-logging", "#");
+				m.BasicConsume(q, true, consumer);
+				while (true)
+					Console.WriteLine(((BasicDeliverEventArgs) consumer.Queue.Dequeue()).Body.AsUtf8String());
+			}
+		}
+	}
+
+	static class Extensions
+	{
+		public static string AsUtf8String(this byte[] args)
+		{
+			return Encoding.UTF8.GetString(args);
+		}
+	}
+}
